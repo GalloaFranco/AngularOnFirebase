935363c4acfcde47686dbb256101b29c41696ea4
galloafranco <galloafranco@gmail.com>
2019-09-16 23:48:38 -0300

    feature: Se crean todos los metodos de nuestro servicio crud. Se incorpora en el spec el AngularFireDatabase.

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
+import {AngularFireDatabase} from 'angularfire2/database';
 import { ProductService } from './product.service';
 
 describe('ProductService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      {provide: AngularFireDatabase}
+    ]
+  }));
 
   it('should be created', () => {
     const service: ProductService = TestBed.get(ProductService);
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
+import {Product} from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  products: AngularFireList<any>;
+  selectedProduct: Product = new Product();
+
+  constructor(private firebase: AngularFireDatabase) {}
+
+  getProducts() {
+    return this.products = this.firebase.list('products');
+  }
+
+  insertProduct(product: Product) {
+    this.products.push({
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+
+  updateProduct(product: Product) {
+    this.products.update(product.key, {
+      name: product.name,
+      category: product.category,
+      location: product.location,
+      price: product.price
+    });
+  }
+
+  deleteProduct(key: string) {
+    this.products.remove(key);
+  }
 }
