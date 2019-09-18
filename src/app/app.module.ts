f3cff29aebe4dfe27d8cb045c0cb57caf4080531
galloafranco <galloafranco@gmail.com>
2019-09-17 21:08:15 -0300

    feature: Modulo de FormsModule y el ProductComponent en el appModule. Template de app.component con los selectores de products y dumb.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,24 @@ import {AppComponent} from './app.component';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireModule} from 'angularfire2';
 import {environment} from '../environments/environment';
-import { ProductsComponent } from './components/products/products.component';
-import { ProductDumbComponent } from './components/products/product-dumb/product-dumb.component';
+import {ProductsComponent} from './components/products/products.component';
+import {ProductDumbComponent} from './components/products/product-dumb/product-dumb.component';
 import {ProductService} from './services/product.service';
+import {ProductComponent} from './components/products/product/product.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    ProductDumbComponent
+    ProductDumbComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     AngularFireDatabaseModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
